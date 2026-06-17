4701606d7176d35ecfa43233f9c55cf26df066a2
Yusuf Tas <info@yusuftas.net>
2026-06-17 20:23:18 +1000

    Trying again to get JS and TOC working

--- assets/js/data/swconf.js
+++
@@ -1,1 +0,0 @@
-const swconf = { cacheName: 'chirpy-1773605821',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
